fc4326266711267ae7d051746aedca256ac60c5a
Gasoile <gasoile@gmail.com>
2023-03-21 11:02:05 +0000

    change modern device tag

--- components/modern_wind/modern_wind.c
+++ components/modern_wind/modern_wind.c
@@ -22,7 +22,7 @@
 #include <string.h>
 #include "modern_wind.h"
 
-static const char *TAG = "MODERN_WIND";
+static const char *TAG = "modern_wind";
 
 #define CHECK(x) do { esp_err_t __; if ((__ = x) != ESP_OK) return __; } while (0)
 #define CHECK_ARG(VAL) do { if (!(VAL)) return ESP_ERR_INVALID_ARG; } while (0)
